7f889618b5273519aa0e83685b7b929c7673c1e8
denis-orbs <101042147+denis-orbs@users.noreply.github.com>
2022-08-29 10:24:04 +0000

    Deploying to gh-pages from @ denis-orbs/interface-v2@a4b8f6767359d8c17e4636e18a1123a79ea9cf78 🚀

---
+++ static/js/17.caea1d06.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpinterface-v2"]=this["webpackJsonpinterface-v2"]||[]).push([[17],{2943:function(n,c){},2993:function(n,c){}}]);
+//# sourceMappingURL=17.caea1d06.chunk.js.map
--- static/js/22.8c91825b.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonpinterface-v2"]=this["webpackJsonpinterface-v2"]||[]).push([[22],{3005:function(e,n){}}]);
-//# sourceMappingURL=22.8c91825b.chunk.js.map
